8ec4b847eee64747d7380fb386ec8372ded9d305
pagrub <pagrub@ttu.ee>
2020-09-08 00:31:23 +0300

    another try

--- PR/pr01_python/python.py
+++ PR/pr01_python/python.py
@@ -1,9 +1,9 @@
-"""This program asks user about his name and year of birth."""
+"""This program shows how old you had been when Python 3 was released."""
 
 name = input("What is your name?")
 year = int(input("Hello, " + name + "! What year were you born in?"))
-persons_age = str(2008 - year)
 python_age = str(year - 2008)
+persons_age = str(2008 - year)
 
 if int(year) <= 2008:
     print("You were " + persons_age + ' years old when Python 3.0 was released')
