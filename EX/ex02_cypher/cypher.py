2ac4a7910c83604ac79c0fdb86ba797de2759d70
pagrub <pagrub@ttu.ee>
2020-09-09 21:55:25 +0300

    2 try

--- EX/ex02_cypher/cypher.py
+++ EX/ex02_cypher/cypher.py
@@ -2,7 +2,7 @@
 
 
 def encode(message: str, key: int) -> str:
-    """alphabet is cipher. alphabet2 is plain. """
+    """alphabet is cipher. alphabet2 is plain."""
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z']
     alphabet2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
