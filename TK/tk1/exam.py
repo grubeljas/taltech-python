ab9c79a8451d553de0a64e035ed9e1f53fc665fe
pagrub <pagrub@ttu.ee>
2020-09-29 14:27:07 +0300

    first

---
+++ TK/tk1/exam.py
@@ -0,0 +1,121 @@
+"""Test 1 (K14)."""
+
+
+def workday_count(days):
+    """
+    Give number of days.
+
+    Return how many of these days are workdays.
+    Workdays are first five days of the weeks, last two are not.
+    Always start from the start of the week.
+
+    workday_count(9) => 7
+    workday_count(3) => 3
+    workday_count(7) => 5
+    workday_count(15) => 11
+
+    :param days: given number of days
+    :return: workdays in given days
+    """
+    whole_week = days // 7
+    add_days = days % 7
+    if add_days == 6:
+        add_days = 5
+    counter = whole_week * 5 + add_days
+    return counter
+
+
+def caught_speeding(speed, is_birthday):
+    """
+    Return which category speeding ticket you would get.
+
+    You are driving a little too fast, and a police officer stops you.
+    Write code to compute the result, encoded as an int value:
+    0=no ticket, 1=small ticket, 2=big ticket.
+    If speed is 60 or less, the result is 0.
+    If speed is between 61 and 80 inclusive, the result is 1.
+    If speed is 81 or more, the result is 2.
+    Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+
+    caught_speeding(60, False) => 0
+    caught_speeding(65, False) => 1
+    caught_speeding(65, True) => 0
+
+    :param speed: Speed value.
+    :param is_birthday: Whether it is your birthday (boolean).
+    :return: Which category speeding ticket you would get (0, 1, 2).
+    """
+    bonus = 0
+    if is_birthday:
+        bonus = 5
+    if speed <= 60 + bonus:
+        return 0
+    if speed > 80 + bonus:
+        return 2
+    else:
+        return 1
+
+
+def first_half(text):
+    """
+    Return the first half of an string.
+
+    The length of the string is even.
+
+    first_half('HaaHoo') => 'Haa'
+    first_half('HelloThere') => 'Hello'
+    first_half('abcdef') => 'abc'
+    """
+    end = len(text) // 2
+    new_text = text[:end]
+    return new_text
+
+
+def last_indices_elements_sum(nums):
+    """
+    Return sum of elements at indices of last two elements.
+
+    Take element at the index of the last element value
+    and take element at the index of the previous element value.
+    Return the sum of those two elements.
+
+    If the index for an element is out of the list, use 0 instead.
+
+    The list contains at least 2 elements.
+
+    last_indices_elements_sum([0, 1, 2, 0]) => 2 (0 + 2)
+    last_indices_elements_sum([0, 1, 1, 7]) => 1 (just 1)
+    last_indices_elements_sum([0, 1, 7, 2]) => 7 (just 7)
+    last_indices_elements_sum([0, 1, 7, 8]) => 0 (indices too large, 0 + 0)
+
+    :param nums: List of non-negative integers.
+    :return: Sum of elements at indices of last two elements.
+    """
+    first = nums[nums[-1]]
+    second = nums[nums[-2]]
+    summa = first + second
+    return summa
+
+
+def max_duplicate(nums):
+    """
+    Return the largest element which has at least one duplicate.
+
+    If no element has duplicate element (an element with the same value), return None.
+
+    max_duplicate([1, 2, 3]) => None
+    max_duplicate([1, 2, 2]) => 2
+    max_duplicate([1, 2, 2, 1, 1]) => 2
+
+    :param nums: List of integers
+    :return: Maximum element with duplicate. None if no duplicate found.
+    """
+    a = -1
+    for i in nums:
+        nums.remove(i)
+        if i in nums:
+            if i > a:
+                a = i
+    if a == -1:
+        return None
+    return a
