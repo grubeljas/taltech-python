ebb617cdc08a1a2a77eeed70dfbf1b4718377ea9
pagrub <pagrub@ttu.ee>
2020-10-04 13:00:32 +0300

    another

--- PR/pr06_twitter/twitter.py
+++ PR/pr06_twitter/twitter.py
@@ -111,30 +111,8 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
             flipped[value].append(key)
     for k, v in flipped.items():
         v = v.sort()
-    sorted_hashtags = sorted(flipped.items(), key=lambda x: x[1])
+    sorted_hashtags = sorted(flipped.items(), key=lambda x: x[1], reverse=True)
     for value in sorted_hashtags:
         for hashtag in value[1]:
             bruh.append(hashtag)
     return bruh
-
-
-if __name__ == '__main__':
-    tweet1 = Tweet("@realDonaldTrump", "Despite the negative press covfefe #bigsmart", 1249, 3)
-    tweet2 = Tweet("@elonmusk", "Technically, alcohol is a solution #bigsmart", 366.4, 233)
-    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 238)
-    tweet4 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #bich", 2192, 238)
-    tweets = [tweet1, tweet2, tweet3, tweet4]
-
-    print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
-
-    filtered_by_popularity = sort_by_popularity(tweets)
-    print(filtered_by_popularity[0].user)  # -> "@CIA"
-    print(filtered_by_popularity[1].user)  # -> "@elonmusk"
-    print(filtered_by_popularity[2].user)  # -> "@realDonaldTrump"
-
-    filtered_by_hashtag = filter_by_hashtag(tweets, "#bigsmart")
-    print(filtered_by_hashtag[0].user)  # -> "@realDonaldTrump"
-    print(filtered_by_hashtag[1].user)  # -> "@elonMusk"
-
-    sorted_hashtags = sort_hashtags_by_popularity(tweets)
-    print(sorted_hashtags)  # -> "#heart"
