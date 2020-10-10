92271e5bf0aca85bcddeddbc1dabb88b07491736
pagrub <pagrub@ttu.ee>
2020-10-10 18:44:23 +0300

    another

--- PR/pr07_train/train.py
+++ PR/pr07_train/train.py
@@ -2,33 +2,74 @@
 
 
 class Train:
+    """Train class."""
+
     def __init__(self, passengers: list, carriages: int, seats_in_carriage: int):
+        """
+        Train constructor.
+
+        :param passengers:
+        :param carriages:
+        :param seats_in_carriage:
+        """
         self.passengers = passengers
         self.carriages = carriages
         self.seats_in_carriage = seats_in_carriage
 
     @property
     def passengers(self) -> list:
+        """
+        Return list of passengers.
+
+        :return:
+        """
         return self._passengers
 
     @property
     def carriages(self) -> int:
+        """
+        Return number of carriages.
+
+        :return:
+        """
         return self._carriages
 
     @property
     def seats_in_carriage(self) -> int:
+        """
+        Return number of seats in carriage.
+
+        :return:
+        """
         return self._seats_in_carriage
 
     def get_seats_in_train(self) -> int:
+        """
+        Return number of seats in train.
+
+        :return:
+        """
         return self.carriages * self.seats_in_carriage
 
     def get_number_of_passengers(self, passengers) -> int:
+        """
+        Count number of passengers.
+
+        :param passengers:
+        :return:
+        """
+        self._passengers = passengers
         a = 0
-        for i in range(len(passengers)):
+        for i in range(len(self._passengers)):
             a += 1
         return a
 
     def get_passengers_in_carriages(self) -> dict:
+        """
+        Check passengers be carriage.
+
+        :return:
+        """
         dict = {}
         if self.carriages == 0:
             return dict
@@ -43,23 +84,54 @@ class Train:
 
     @passengers.setter
     def passengers(self, value_list: list):
+        """
+        Set passengers.
+
+        :param value_list:
+        :return:
+        """
         self._passengers = value_list
 
     @carriages.setter
     def carriages(self, value: int):
+        """
+        Set carriages.
+
+        :param value:
+        :return:
+        """
         self._carriages = value
 
     @seats_in_carriage.setter
     def seats_in_carriage(self, value: int):
+        """
+        Set seats.
+
+        :param value:
+        :return:
+        """
         self._seats_in_carriage = value
 
 
 class Passenger:
+    """Passenger class."""
+
     def __init__(self, passenger_id: str, seat: str):
-        self.passenger_id = passenger_id
+        """
+        Passenger constructor.
+
+        :param passenger_id:
+        :param seat:
+        """
+        self.id = passenger_id
         self.seat = seat
 
     def __dict__(self):
+        """
+        Return dictionary with passenger data.
+
+        :return:
+        """
         return {'id': self.passenger_id, 'seat': self.seat}
 
 
