48f6ceda5e777e784f71f7c960412ecdfa97bbc3
pagrub <pagrub@ttu.ee>
2020-11-01 16:40:28 +0200

    another

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,1 +1,61 @@
-"""Wat"""
+"""Wat."""
+
+
+def function_a(n: int) -> int:
+    return -1
+
+
+def function_b(n: int) -> int:
+    return -1
+
+
+def function_c(n: int) -> int:
+    return -1
+
+
+def function_d(n: int) -> int:
+    return -1
+
+
+def function_e(n: int) -> int:
+    return -1
+
+
+def function_f(n: int) -> int:
+    return -1
+
+
+def function_g(n: int) -> int:
+    return -1
+
+
+def function_h(n: int) -> int:
+    return -1
+
+
+def function_i(n: int) -> int:
+    return -1
+
+
+def function_j(n: int) -> int:
+    return -1
+
+
+def function_sum(n: int) -> int:
+    return -1
+
+
+def function_multiple(n: int) -> int:
+    return -1
+
+
+def function_mod(n: int) -> int:
+    return -1
+
+
+def function_composition(n: int) -> int:
+    return -1
+
+
+def function_meta_composition(n: int) -> int:
+    return -1
