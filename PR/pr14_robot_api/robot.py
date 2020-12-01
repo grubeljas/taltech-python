ac719a3ab7a9ba1c95b678c9d8de5123c13e9e9a
pagrub <pagrub@ttu.ee>
2020-12-01 17:52:00 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -3,6 +3,7 @@ from PiBot import PiBot
 
 robot = PiBot()
 robot.set_wheels_speed(30)
-if robot.get_front_middle_laser() < 10.0:
-    robot.set_wheels_speed(0)
+if robot.get_line_sensors() < 200:
+    robot.set_wheels_speed(-30)
+    robot.sleep(1)
 robot.done()
