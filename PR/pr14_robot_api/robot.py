d44b6c3884f9bb9ee8a1bf65fd18c2ad33dfae1a
pagrub <pagrub@ttu.ee>
2020-12-01 18:03:10 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -2,6 +2,7 @@
 from PiBot import PiBot
 
 robot = PiBot()
-while robot.get_line_sensors()[0][0] > 200:
-    robot.set_wheels_speed(30)
+robot.set_wheels_speed(30)
+robot.sleep(5)
 robot.set_wheels_speed(0)
+
