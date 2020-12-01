612f1cfab362f1aa116e7e8af80ccae72d730dff
pagrub <pagrub@ttu.ee>
2020-12-01 17:37:16 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -3,7 +3,6 @@ from PiBot import PiBot
 
 robot = PiBot()
 robot.set_wheels_speed(30)
-distance_from_object = robot.get_front_middle_laser()
 while True:
     if robot.get_front_middle_laser() < 10.0:
         robot.set_wheels_speed(0)
