bfb236de67bda40266619be664345805441120be
pagrub <pagrub@ttu.ee>
2020-12-01 17:28:41 +0200

    second
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -2,7 +2,10 @@
 from PiBot import PiBot
 
 robot = PiBot()
+distance_from_object = robot.get_front_middle_laser()
 robot.set_wheels_speed(30)
-robot.sleep(2)
+while distance_from_object > 0.2:
+    distance_from_object = robot.get_front_middle_laser()
+    robot.sleep(0.05)
 robot.set_wheels_speed(0)
 robot.done()
