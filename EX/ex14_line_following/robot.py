de7736c5ed8f8553d7f2155752093ea3e0068ad4
pagrub <pagrub@ttu.ee>
2020-12-03 01:16:29 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -7,7 +7,7 @@ robot.sleep(1)
 robot.get_left_line_sensors()
 for i in range(100):
     robot.set_left_wheel_speed(-30)
-    robot.set_right_wheel_speed(30)
+    robot.set_right_wheel_speed(60)
     print(robot.get_rotation())
     robot.sleep(1)
     robot.set_wheels_speed(30)
