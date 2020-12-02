70c2f0539ca53234b79edd2a94adaff0448c8f5d
pagrub <pagrub@ttu.ee>
2020-12-02 03:57:30 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -3,12 +3,10 @@ from PiBot import PiBot
 
 robot = PiBot()
 robot.set_wheels_speed(30)
-if robot.get_rear_right_side_ir() < 400:
-    robot.set_wheels_speed(30)
-    robot.sleep(1)
-    robot.set_left_wheel_speed(-30)
-    robot.set_right_wheel_speed(30)
-    robot.sleep(1)
+robot.sleep(1)
+robot.set_left_wheel_speed(-30)
+robot.set_right_wheel_speed(30)
+robot.sleep(1)
 robot.set_wheels_speed(30)
 robot.sleep(1)
 robot.set_wheels_speed(0)
