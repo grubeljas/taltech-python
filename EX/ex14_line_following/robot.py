6a81b6519c21f47a74df030de8d01e68b67957df
pagrub <pagrub@ttu.ee>
2020-12-03 16:03:35 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -3,7 +3,7 @@ from PiBot import PiBot
 
 robot = PiBot()
 print(robot.get_line_sensors())
-for i in range(39):
+for i in range(200):
     robot.set_wheels_speed(10)
     robot.sleep(0.05)
     print(robot.get_line_sensors())
