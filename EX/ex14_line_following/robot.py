efa4ecbaaa8adb2a4f6ed1d3c81f12a2ed261735
pagrub <pagrub@ttu.ee>
2020-12-03 02:06:09 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -3,9 +3,6 @@ from PiBot import PiBot
 
 robot = PiBot()
 for i in range(4):
-    robot.set_left_wheel_speed(-25)
-    robot.set_right_wheel_speed(25)
-    robot.sleep(1)
     robot.set_wheels_speed(12)
     robot.sleep(3)
     robot.set_right_wheel_speed(-25)
