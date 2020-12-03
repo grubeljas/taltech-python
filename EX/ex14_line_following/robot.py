92ff0af3419da3e227ae34468a447ddb4ce40817
pagrub <pagrub@ttu.ee>
2020-12-03 16:37:19 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -17,8 +17,9 @@ while 0 in robot.get_line_sensors():
         robot.sleep(0.05)
         print(robot.get_line_sensors())
         i -= 1
-    while robot.get_line_sensors()[2] < 101:
+    while robot.get_third_line_sensor_from_left() < 101 or i > - 5:
         robot.set_wheels_speed(10)
         robot.sleep(0.05)
         print(robot.get_line_sensors())
+        i -= 1
 robot.set_wheels_speed(0)
