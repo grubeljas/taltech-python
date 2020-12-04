0c7a215b8a11e5afb4ee39d96f0a0e745e0c98d6
pagrub <pagrub@ttu.ee>
2020-12-04 22:00:11 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -4,23 +4,15 @@ from PiBot import PiBot
 robot = PiBot()
 
 a = 0
-while 0 in robot.get_line_sensors() or 100 >  robot.get_line_sensors()[5]:
-    i = 0
+while 0 in robot.get_line_sensors() or 100 > robot.get_line_sensors()[5]:
     if robot.get_third_line_sensor_from_left() > 0:
         robot.set_right_wheel_speed(-2)
         robot.set_left_wheel_speed(2)
         robot.sleep(0.005)
         print(robot.get_line_sensors())
-        i = 1
     if robot.get_third_line_sensor_from_left() == 0:
         robot.set_wheels_speed(8)
         robot.sleep(0.01)
         print(robot.get_line_sensors())
-        i = 1
-    if i == 0:
-        print('MMMMMMMMMMMMMMMMMMM')
-        a += 1
-        if a == 20:
-            break
 robot.set_wheels_speed(0)
 robot.done()
