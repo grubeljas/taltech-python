16a7198ba18d6a5f60c1b58ce4bb686bd4eef678
pagrub <pagrub@ttu.ee>
2020-12-04 21:12:32 +0200

    style
    Signed-off-by: pagrub <pagrub@ttu.ee>

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -2,13 +2,19 @@
 from PiBot import PiBot
 
 robot = PiBot()
+
 while 0 in robot.get_line_sensors():
-    while robot.get_third_line_sensor_from_left() > 100:
+    i = 0
+    while robot.get_third_line_sensor_from_left() > 0:
         robot.set_right_wheel_speed(-20)
-        robot.set_left_wheel_speed(43)
+        robot.set_left_wheel_speed(20)
         robot.sleep(0.05)
+        i = 1
     while robot.get_third_line_sensor_from_left() == 0:
         robot.set_wheels_speed(10)
         robot.sleep(0.05)
+        i = 1
+    if i == 0:
+        break
 robot.set_wheels_speed(0)
 robot.done()
